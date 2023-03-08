831e1a27845e8a480d886ceb3b8376502370967f
Sarut.r <sarut.r@sea.com>
2023-03-08 15:06:37 +0700

    Change yaml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   setTimeout(function() {
     document.getElementsByClassName("loading")[0].style.display = 'none';
+    document.getElementsByClassName("page")[0].style.display = 'flex';
   }, 2000);
 };
